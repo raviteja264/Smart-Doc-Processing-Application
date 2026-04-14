a94bd58db63adaead8c72a15f3769aa44d4bb279
raviteja264 <pasupuletiraviteja264@gmail.com>
2026-04-14 15:44:56 +0530

    modified a new endpoint api/v1/ping

--- app/api/v1/endpoints/ping.py
+++ app/api/v1/endpoints/ping.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/ping", tags=["ping"])
+router = APIRouter(tags=["ping"])
 
 @router.get("/ping", status_code=200)
 async def ping():
+    """
+    Endpoint to check if the API is responsive.
+    
+    """
     return {"message": "pong"}
